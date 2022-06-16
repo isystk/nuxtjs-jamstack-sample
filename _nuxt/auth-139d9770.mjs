2e5991af7e66d7abc30cf3a657b91d0b5c4f383b
isystk <isystk@users.noreply.github.com>
2022-06-16 13:05:20 +0000

    deploy: 12f2b13409324a2b9a6eb7e20475d852449e4490

---
+++ _nuxt/auth-139d9770.mjs
@@ -0,0 +1,1 @@
+import{R as e}from"./entry-63218459.mjs";var d=e((o,t,a)=>{console.log("middleware auth")});export{d as default};
--- _nuxt/url-a702e221.mjs
+++
@@ -1,1 +0,0 @@
-const o=t=>["",t].join(""),n={TOP:o("/"),LOGIN:o("/login"),POSTS:o("/posts")};export{n as U};
